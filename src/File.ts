1469088bdac00af3c1d6c93bdcc9a2e25a891c1e
chipseater <jeanpauldemalet@yahoo.fr>
2023-04-06 14:26:45 +0000

    [File] Translated a file into raw hex

--- src/File.ts
+++ src/File.ts
@@ -1,30 +1,12 @@
+import fs from 'fs'
+
 export default class CustomFile {
-    name: string
-    type: string
-    size: number
+    path: string
     content: string
 
-    constructor(path: string, type: string, size: number) {
-        this.name = path
-        this.type = type
-        this.size = size
-        this.content = this.getContent()
-    }
-
-    getContent() {
-        const file = new Blob(['hello world']); // your file
-
-
-        const fr = new FileReader();
-        fr.addEventListener('load', function () {
-            var u = new Uint8Array(this.content),
-                a = new Array(u.length),
-                i = u.length;
-            while (i--) // map to hex
-                a[i] = (u[i] < 16 ? '0' : '') + u[i].toString(16);
-            u = null; // free memory
-            console.log(a); // work with this
-        })
-        fr.readAsArrayBuffer(file);
+    constructor(path: string) {
+        this.path = path
+        this.content = fs.readFileSync(this.path, 'hex')
+        
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { HelloWorld } from './test.js'
+import CustomFile from './File.js'
 
-console.log(HelloWorld)
+const test_file = new CustomFile('src/File.ts')
+
+console.log(test_file.content)
