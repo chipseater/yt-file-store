e4d5309dcf63768850fbdcce196a53a066345221
chipseater <jeanpauldemalet@yahoo.fr>
2023-05-07 09:00:20 +0000

    [Deps] fixed static-ffmpeg

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
+import ffmpeg from 'fluent-ffmpeg'
+import { fileURLToPath } from 'url'
+import path from 'path'
 import File from './File.js'
 import Film from './Film.js'
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+ffmpeg.setFfmpegPath(path.join(__dirname, '../node_modules/ffmpeg-static/ffmpeg'))
 
 const file = new File('song.mp3')
 const film = new Film('song')
 
-// ;(await file.toFilm()).command.run()
-film.toFile()
+;(await file.toFilm()).command.on('end', () => {    
+    film.toFile()
+}).run()
